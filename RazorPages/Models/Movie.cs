7167964e3b9d546a1082545ea26a379a6b0d3bab
Sharif M. Tarik <s.tarik@cimplux.com>
2021-03-03 12:35:59 +0600

    Update CRUD pages

--- RazorPages/Models/Movie.cs
+++ RazorPages/Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPages.Models
 {
@@ -7,8 +8,10 @@ namespace RazorPages.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        [DataType(DataType.Date)] public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Release Date")] [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
