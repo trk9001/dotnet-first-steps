e094bc4c74e6da296d1b79ead8e61da9df838642
Sharif M. Tarik <s.tarik@cimplux.com>
2021-03-03 15:39:00 +0600

    Add migrations

---
+++ RazorPages/Migrations/20210303070846_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPages.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                "Movie",
+                table => new
+                {
+                    ID = table.Column<int>("INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>("TEXT", nullable: true),
+                    ReleaseDate = table.Column<DateTime>("TEXT", nullable: false),
+                    Genre = table.Column<string>("TEXT", nullable: true),
+                    Price = table.Column<decimal>("decimal(18, 2)", nullable: false),
+                    Rating = table.Column<string>("TEXT", nullable: true)
+                },
+                constraints: table => { table.PrimaryKey("PK_Movie", x => x.ID); });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                "Movie");
+        }
+    }
+}
