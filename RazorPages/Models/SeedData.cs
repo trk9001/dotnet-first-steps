dcd0122d0cb71f2c8abf9253c33baafee0eaa0c6
Sharif M. Tarik <s.tarik@cimplux.com>
2021-03-03 11:29:02 +0600

    Add a DB seeder

---
+++ RazorPages/Models/SeedData.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPages.Data;
+
+namespace RazorPages.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesContext(
+                serviceProvider.GetRequiredService<DbContextOptions
+                    <RazorPagesContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
